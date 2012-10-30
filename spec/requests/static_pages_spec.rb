c3013e120c6dfa5a62afb1e41e91078b1ad58612
Tyson <tyson1704@gmail.com>
2012-10-30 19:24:15 +1000

    Add a full title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,15 +4,20 @@ describe "Static pages" do
 
   describe "Home page" do
 
-    it "should have the content 'Not For Profit'" do
+    it "should have the h1 'Not For Profit'" do
       visit '/static_pages/home'
-      page.should have_content('Not For Profit')
+      page.should have_selector('h1', :text => 'Not For Profit')
     end
 
-    it "should have the title 'Home'" do
+    it "should have the base title" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                :text => "Not For Profit | Home")
+                                :text => "Not For Profit")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
 
