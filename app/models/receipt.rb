03650d8c29dc609c01af16c3d39939cf789d41fc
bigeareddog <tyson1704@gmail.com>
2013-11-19 13:20:27 +1000

    applied datatables to applicable grids

--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,3 +1,7 @@
 class Receipt < ActiveRecord::Base
   attr_accessible :bin_location, :complete_order, :description, :label_qty, :oum, :part_no, :receipt_ref, :stock_code
+
+ validates :bin_location, presence: true 
+ validates :receipt_ref, presence: true 
+ validates :label_qty, presence: true,numericality: { only_integer: true, :greater_than_or_equal_to => 0 }  
 end
